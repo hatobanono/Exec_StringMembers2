e89c4f5e295e9a882dbc2b3fc2d43c17b728dcfb
hatobanono <chris00iq@gmail.com>
2022-10-20 12:00:39 +0800

    加入專案檔案。

---
+++ Q3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string account = "aLLeN kUO";
+			string[] words = account.ToLower().Split(' ');
+
+			for (int i = 0; i < words.Length; i++) 
+			{
+				words[i] = words[i].Substring(0, 1).ToUpper() + words[i].Substring(1);
+			}
+
+			string reslut = string.Join(" ", words);
+			Console.WriteLine(reslut);
+
+		}
+	}
+}
---
+++ Q7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double n1 = 1.234567;
+			double n2 = 123.4567;
+
+			double sum = n1 + n2;
+
+			Console.WriteLine(sum.ToString("0.00"));
+		}
+	}
+}
